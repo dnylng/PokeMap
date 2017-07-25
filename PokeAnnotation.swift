2c45f813b95efcef3395e8ed7585412ef4917034
dnylng <iamdannyluong@gmail.com>
2017-07-25 10:35:52 -0400

    Integrated firebase database and set up the map

---
+++ PokeAnnotation.swift
@@ -0,0 +1,9 @@
+//
+//  PokeAnnotation.swift
+//  PokeMap
+//
+//  Created by Danny Luong on 7/25/17.
+//  Copyright © 2017 dnylng. All rights reserved.
+//
+
+import Foundation
--- ViewController.swift
+++ ViewController.swift
@@ -7,13 +7,79 @@
 //
 
 import UIKit
+import MapKit
+import GeoFire
+import FirebaseDatabase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    var mapHasCenteredOnce = false
+    var geoFire: GeoFire!
+    var geoFireRef: DatabaseReference
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        mapView.delegate = self
+        mapView.userTrackingMode = MKUserTrackingMode.follow
+
+        // Create a reference to the data base and init GeoFire with that database reference
+        geoFireRef = Database.database().reference()
+        geoFire = GeoFire(firebaseRef: geoFireRef)
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        locationAuthStatus()
+    }
+    
+    func locationAuthStatus() {
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            mapView.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            mapView.showsUserLocation = true
+        }
+    }
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, 2000, 2000)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        if let location = userLocation.location {
+            if !mapHasCenteredOnce {
+                centerMapOnLocation(location: location)
+                mapHasCenteredOnce = true
+            }
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        var annotationView: MKAnnotationView?
+        
+        if annotation.isKind(of: MKUserLocation.self) {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "User")
+            annotationView?.image = UIImage(named: "ash")
+        }
+        
+        return annotationView
+    }
+    
+    func createSighting(forLocation location: CLLocation, withPokemon pokeId: Int) {
+        geoFire.setLocation(location, forKey: "\(pokeId)")
+    }
+    
+    @IBAction func spotRandomPokemon(_ sender: Any) {
+    }
+    
 }
 
